33cd7253d07372855e44de9a377bb172a3209464
Willian Araujo <willian.araujo1994@gmail.com>
2023-02-22 10:45:51 -0300

    criando tela de game over

--- app/validacao.js
+++ app/validacao.js
@@ -10,6 +10,7 @@ function verificaSeOChutePossuiValorValido(chute) {
             <h3>Pressione o botão para jogar novamente</h3>
             <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
             `
+            
             document.body.style.backgroundColor = 'black';
         }
 
