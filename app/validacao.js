ef677a4bf4d94bfba23faee0baf848c451a1a5a5
Willian Araujo <willian.araujo1994@gmail.com>
2023-02-22 12:12:00 -0300

    criando tela de game over

--- app/validacao.js
+++ app/validacao.js
@@ -2,7 +2,21 @@ function verificaSeOChutePossuiValorValido(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>';
+
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = `
+            
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+            `
+
+            document.body.style.backgroundColor = 'black';
+        }
+
+        else {
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+        }
     }
 
     if (numeroForMaiorOuMenorQueOVlaorPermitido(numero)) {
